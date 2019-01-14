c0404b45aeb27b531bde3babd78a12579bd92a2f
abraham esparza <abraham.esparza.m010@gmail.com>
2019-01-13 19:39:32 -0600

    PROTOBUF - NodeJS - Se creo la persona

--- backend/nodejs/app.js
+++ backend/nodejs/app.js
@@ -1,10 +1,19 @@
 const http = require('http');
 
-require("google-protobuf");
-require('./messages/person_pb')
+require('google-protobuf');
+require('./messages/person_pb');
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+
+// Creación de la instancia del message
+const person = new proto.Person();
+
+// Asignación de valores
+person.setName("Nombre1");
+person.setEmail("correo@electronico.com");
+person.getPhoneList().push( new proto.Person.PhoneNumber(["449 123 45 67", proto.Person.PhoneType.HOME]))
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
