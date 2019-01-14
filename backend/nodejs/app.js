3959f07129410fcd96a176ca8b21fcdca07ec6fd
abraham esparza <abraham.esparza.m010@gmail.com>
2019-01-13 19:47:42 -0600

    PROTOBUF - NodeJS - Se visualiza el message

--- backend/nodejs/app.js
+++ backend/nodejs/app.js
@@ -23,4 +23,5 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(person.toObject());
 });
