ce6e6d4b31298d7681615c0d226a1aa053189d51
abraham esparza <abraham.esparza.m010@gmail.com>
2019-01-17 19:56:42 -0600

    PROTOBUF - NodeJS - Se agrego el metodo para testear con una peiticion GET

--- backend/nodejs/server_test.js
+++ backend/nodejs/server_test.js
@@ -1,4 +1,16 @@
-var express = require('express');
+var request = require('request');
 
 require('google-protobuf');
 require('./messages/person_pb');
+
+request('http://127.0.0.1:3000/message', function (error, response, body) {
+
+    console.log('test get http://127.0.0.1:3000/message');
+
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    console.log('body:', body);
+
+    const person = proto.Person.deserializeBinary(new Buffer(body));
+    console.log('persona:', person.toObject());
+});
