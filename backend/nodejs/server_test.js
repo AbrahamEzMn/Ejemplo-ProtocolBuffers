21b5c8378daa80d1ff6f06595c482aa6ec20cc82
abraham esparza <abraham.esparza.m010@gmail.com>
2019-01-17 20:14:04 -0600

    PROTOBUF - NodeJS - Se agrego el resultado del test post

--- backend/nodejs/server_test.js
+++ backend/nodejs/server_test.js
@@ -14,3 +14,23 @@ request('http://127.0.0.1:3000/message', function (error, response, body) {
     const person = proto.Person.deserializeBinary(new Buffer(body));
     console.log('persona:', person.toObject());
 });
+
+// Creación de la instancia del message
+const person = new proto.Person();
+
+// Asignación de valores
+person.setName("Nombre 2");
+person.setEmail("correo2@electronico2.com");
+person.getPhoneList().push( new proto.Person.PhoneNumber(["449 123 45 67", proto.Person.PhoneType.HOME]))
+
+request.post({
+  headers:  {'content-type': 'application/x-protobuf'},
+  url:     'http://127.0.0.1:3000/message',
+  body:    person.serializeBinary()
+}, function(error, response, body){
+  console.log('\n\rtest post http://127.0.0.1:3000/message');
+
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
