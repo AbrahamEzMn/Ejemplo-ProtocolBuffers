0cef9a29795ce46364cb349e0a747e93255838e3
abraham esparza <abraham.esparza.m010@gmail.com>
2019-01-26 17:27:45 -0600

    PROTOBUF - DART - Se documento los metodos GET y POST

--- backend/dart/bin/server.dart
+++ backend/dart/bin/server.dart
@@ -1,10 +1,4 @@
 
-// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
-// Replies "Hello, world!" to all requests.
-// Use the URL localhost:4040 in your browser.
 import 'dart:io';
 import 'dart:convert';
 import 'dart:async';
