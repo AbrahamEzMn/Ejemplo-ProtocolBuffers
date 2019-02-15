470f25b2ec3569c9ee68eeddb02a95dd413900c1
Abraham Esparza Moreno <abraham.esparza.m010@gmail.com>
2019-02-14 21:11:11 -0600

    PROTOBUF - NodeJS - Se agrego la configuracion para evitar problemas de acceso con CORS

--- backend/nodejs/server.js
+++ backend/nodejs/server.js
@@ -5,7 +5,17 @@ require('./messages/person_pb');
 
 
 var app = express();
-app.use(bodyParser.raw({type: 'application/x-protobuf'}))
+app.use(bodyParser.raw({type: 'application/x-protobuf'}));
+
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 /**
  * Lanzamos el servidor.
