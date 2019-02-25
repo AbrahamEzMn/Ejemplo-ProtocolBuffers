012c1d48f0dc06868c0e1a6ba1d8a7b6336cebcd
Abraham Esparza Moreno <abraham.esparza.m010@gmail.com>
2019-02-25 00:59:39 -0600

    PROTOBUF - XAMARIN - Se agregaron los mentodos necesario

--- frontend/android-xamarin/android-xamarin/MainActivity.cs
+++ frontend/android-xamarin/android-xamarin/MainActivity.cs
@@ -3,6 +3,13 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using System.Threading.Tasks;
+using System.Net;
+using System;
+using System.IO;
+using Google.Protobuf;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace android_xamarin
 {
@@ -15,5 +22,50 @@ namespace android_xamarin
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
         }
+
+        // Gets weather data from the passed URL.
+        private async Task<byte[]> FetchWeatherAsync(string url)
+        {
+            HttpClient client = new HttpClient();
+
+            var uri = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-protobuf"));
+
+            // Send the request to the server and wait for the response:
+            using (HttpResponseMessage response = await client.GetAsync(uri))
+            {
+                byte[] stream = await response.Content.ReadAsByteArrayAsync();
+                return stream;
+            }
+
+        }
+
+        public async Task<string> Get(string url)
+        {
+            // Creamos a la persona.
+            Person person = new Person
+            {
+                Name = "Nombre de la persona",
+                Email = "correo@electronico.com",
+                Id = 1
+            };
+            person.Phone.Add(new PhoneNumber { Number = "4491234657", Type = PhoneType.Mobile });
+            person.Phone.Add(new PhoneNumber { Number = "3216549871", Type = PhoneType.Work });
+
+
+            var uri = new Uri(url);
+
+            var client = new HttpClient();
+            var content = new ByteArrayContent(person.ToByteArray());
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/x-protobuf");
+
+            // Send the request to the server and wait for the response:
+            using (HttpResponseMessage response = await client.PostAsync(uri, content))
+            {
+                string stream = await response.Content.ReadAsStringAsync();
+                return stream;
+            }
+        }
+
     }
 }
